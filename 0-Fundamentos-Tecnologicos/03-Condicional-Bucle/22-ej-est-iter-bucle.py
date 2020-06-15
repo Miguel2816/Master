eeb11db79cd8901a0c60964f9716fd8d2f46169e
Miguel Pérez Caro <miguel.perez.caro@hotmail.com>
2020-06-15 21:15:46 +0200

    Commit Ejercicios Week 3

---
+++ 0-Fundamentos-Tecnologicos/03-Condicional-Bucle/22-ej-est-iter-bucle.py
@@ -0,0 +1,43 @@
+# Fecha: 15/06/2020
+
+# EJERCICIOS CON RANGE
+
+# Primer ejercicio: mostrar en pantalla los números del 1 al 20
+
+for i in range(1,21):
+    print(i)
+
+print('\n-------------------------------------------------\n')
+
+# Segundo ejercicio: mostrar en pantalla del 3 al 30 de 3 en 3
+
+for i in range(3,31,3):
+    print(i)
+
+print('\n-------------------------------------------------\n')
+
+# Tercer ejercicio: crea una lista con los números anteriores
+
+lista_tres = []
+for i in range(3,31,3):
+    lista_tres.append(i)
+print(lista_tres)
+
+print('\n-------------------------------------------------\n')
+
+# Cuarto ejercicio: mostrar por pantalla el cubo de los números del 1 al 10
+
+lista_cube_tres = []
+for i in range(1,11):
+    print(i)
+    lista_cube_tres.append(i)
+print(lista_cube_tres)
+
+print('\n-------------------------------------------------\n')
+
+# EJERCICIOS CON ESTRUCTURAS REPETITIVAS
+
+# Primer ejercicio: pide un número al usuario y muestra su tabla de multiplicar
+
+
+
