f43a84cced1725951ddbad97a90952a812cafd1b
Miguel Pérez Caro <miguel.perez.caro@hotmail.com>
2020-06-16 20:33:07 +0200

    Exercises Week 3

--- 0-Fundamentos-Tecnologicos/03-Condicional-Bucle/22-ej-est-iter-bucle.py
+++ 0-Fundamentos-Tecnologicos/03-Condicional-Bucle/22-ej-est-iter-bucle.py
@@ -39,5 +39,107 @@ print('\n-------------------------------------------------\n')
 
 # Primer ejercicio: pide un número al usuario y muestra su tabla de multiplicar
 
+numero = abs(int(input("Introduce un número para calcular su tabla de multiplicar: ")))
+print("La tabla de multiplicar del {} es:".format(numero))
+for i in range(1,11):
+    mult = numero*i
+    print("{} * {} = {}".format(numero,i,mult))
 
+print('\n-------------------------------------------------\n')
+
+# Segundo ejercicio: pide nombre y veces que quiere que le salude. Imprime el saludo tantas veces como diga
+
+nombre = input("Introduce su nombre: ")
+veces = int(input("Introduce las veces que quieres que le salude: "))
+
+for i in range(1,veces+1):
+    print("Hola {}".format(nombre))
+
+print('\n-------------------------------------------------\n')
+
+# Tercer ejercicio: Suma los 100 primeros y muestra el resultado
+
+suma = 0
+
+for i in range(1,101):
+    suma += i
+print("La suma de los 100 primeros número es:", suma)
+
+
+# Cuarto ejercicio: Pide números al usuario ahsta que escriba un número negativo. Imprime cuantos positivos excribió
+
+numero = int(input('Por favor, introduce un número: '))
+pos_lista = []
+while numero >= 0:
+    pos_lista.append(numero)
+    numero = int(input('Por favor, introduce un número: '))
+print("El usuario ha escrito {} números positivos".format(len(pos_lista)))
+
+print('\n-------------------------------------------------\n')
+
+# Quinto ejercicio: User, admin y contraseña 1234&. Hasta que el usuario no escriba la contraseña, preguntar de nuevo hasta 3 intentos
+
+correct_user = 'admin'
+correct_password = '1234&'
+
+user = input('Introduce el usuario: ')
+password = input('Introduce la contraseña: ')
+
+intentos = [1]
+
+while (correct_user != user or correct_password != password) and len(intentos) <= 3:
+    print(len(intentos))
+    if correct_user != user and correct_password != password:
+        user = input('Usuario y contraseña incorrectos. Introduce el usuario de nuevo: ')
+        password = input('Introduce la contraseña: ')
+        intentos.append(1)
+    elif correct_user != user and correct_password == password:
+        user = input('Usuario incorrecto. Introduce el usuario de nuevo: ')
+        intentos.append(1)
+    elif correct_user == user and correct_password != password:
+        password = input('Contraseña incorrecta. Introduce la contraseña de nuevo: ')
+        intentos.append(1)
+
+if correct_user == user and correct_password == password:
+    print('Usuario y Contraseña correctas!')
+else:
+    print('Número de intentos agotado')
+    
+print('\n-------------------------------------------------\n')
+
+
+# EJERCICIOS CON ESTRUCTURAS SELECTIVAS Y REPETITIVAS
+
+# Primer ejercicio: crea lista magicians y muestra de forma iterativa
+
+magicians = ['alice', 'carolina', 'anne']
+for i in magicians:
+    print(i)
+
+print('\n-------------------------------------------------\n')
+
+# Segundo ejercicio: crea lista favorite ingredients
+
+favorite_ingredients = ['pepperoni', 'hawaiian', 'veggie']
+for i in favorite_ingredients:
+    print('I love {} in pizza'.format(i))
+print('I love all pizzas')
+
+print('\n-------------------------------------------------\n')
+
+# EJERCICIOS ESTRUCTURAS DE CONTROL Y TIPOS DE DATOS
+
+# Primer ejercicio: crea un tupla con los meses del año, pide números al usuario, y muestra el contenido de esa tupla o error
+
+numero = int(input('Introduce un número: '))
+
+tupla = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+
+while numero != 0:
+    try:
+        print(tupla[numero])
+        numero = int(input('Introduce un número: '))
+    except Exception as e:
+        print(e)
+        numero = int(input('Introduce un número: '))
 
