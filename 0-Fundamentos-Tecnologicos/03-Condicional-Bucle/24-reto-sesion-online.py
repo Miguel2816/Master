7060374289db90fe27cd02476606314f9528dcba
Miguel Pérez Caro <miguel.perez.caro@hotmail.com>
2020-06-28 13:57:41 +0200

    Update 24-reto-sesion-online.py

--- 0-Fundamentos-Tecnologicos/03-Condicional-Bucle/24-reto-sesion-online.py
+++ 0-Fundamentos-Tecnologicos/03-Condicional-Bucle/24-reto-sesion-online.py
@@ -1,36 +1,46 @@
 # Fecha: 20/06/2020
 import random
+import textwrap
 
-historia = 'Videojuego primera mision'
-print(historia)
-jugar = 'si'
-while jugar == 'si':
+if __name__ == '__main__':
 
-    lista_cabanas = []
-    opciones = ('Enemigo', 'Amigo', 'Desocupado')
+    ancho = 70
+    line = '-'*70
 
-    for i in range(0,5):
-        lista_cabanas.append(random.choice(opciones))
+    print(line)
+    print('Game of Thrones v0.0.1')
+    print(line)
 
-    # tupla = tuple([(1,lista_cabanas[0]), (2,lista_cabanas[1]), (3,lista_cabanas[2]), (4,lista_cabanas[3]), (5,lista_cabanas[4])])
-    # tupla = ([(1,lista_cabanas[0]), (2,lista_cabanas[1]), (3,lista_cabanas[2]), (4,lista_cabanas[3]), (5,lista_cabanas[4])])
-    #diccionario = {'Cabana1': lista_cabanas[0],
-    #               'Cabana2': lista_cabanas[1],
-    #               'Cabana3': lista_cabanas[2],
-    #               'Cabana4': lista_cabanas[3],
-    #               'Cabana5': lista_cabanas[4]}
+    historia = "Perteneces a la Guardia de la Noche." \
+                   "Llevas semanas andando al otro lado del muro," \
+                   "en una expedición para detectar posibles salvajes." \
+                   "Llegas a un poblado con 5 cabañas." \
+                   "Debes elegir en qué cabaña entrarás." \
+                   "Si está vacía podrás descansar... pero" \
+                   "corres el riesgo de encontrarte con un salvaje y morir…"
 
-    lista = [(1,lista_cabanas[0]), (2,lista_cabanas[1]), (3,lista_cabanas[2]), (4,lista_cabanas[3]), (5,lista_cabanas[4])]
+    print(textwrap.fill(historia, width=70))
 
-    num_cabana = int(input('Introduce el numero de cabana del 1 al 5: '))
+    jugar = 'si'
+    while jugar == 'si':
 
-    cabana_escogida = lista[num_cabana-1]
-    opcion=cabana_escogida[1]
+        lista_cabanas = []
+        opciones = ('Enemigo', 'Amigo', 'Desocupado')
 
-    if opcion == 'Enemigo':
-        print('Has perdido. Un salvaje acaba de matarte.')
-    elif opcion == 'Amigo':
-        print('Has ganado. Puedes descansar.')
-    else:
-        print('Has ganado. Puedes descansar')
-    jugar = input('Quieres jugar otra vez? (Si/No): ').lower()
+        for i in range(0,5):
+            lista_cabanas.append(random.choice(opciones))
+
+        lista = [(1,lista_cabanas[0]), (2,lista_cabanas[1]), (3,lista_cabanas[2]), (4,lista_cabanas[3]), (5,lista_cabanas[4])]
+
+        num_cabana = int(input('Introduce el numero de cabana del 1 al 5: '))
+
+        cabana_escogida = lista[num_cabana-1]
+        opcion=cabana_escogida[1]
+
+        if opcion == 'Enemigo':
+            print('Has perdido. Un salvaje acaba de matarte.')
+        elif opcion == 'Amigo':
+            print('Has ganado. Puedes descansar.')
+        else:
+            print('Has ganado. Puedes descansar')
+        jugar = input('Quieres jugar otra vez? (Si/No): ').lower()
